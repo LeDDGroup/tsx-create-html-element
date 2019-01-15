74ce55b2a45c221b5de05085123eba45659658b5
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-01-14 20:45:05 -0500

    feat: support function elements

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 /// <reference path="global.d.ts" />
-export function createElement<T extends keyof HTMLElementTagNameMap>(
-  name: T,
-  props: HTMLElementTagNameMap[T],
-  ...rest: Node[]
+
+export type Properties<P> = P & { children?: any };
+export interface FunctionElement<P> {
+  (props: Properties<P>): JSX.Element;
+}
+
+export function createElement<T extends keyof HTMLElementTagNameMap, L>(
+  name: T | FunctionElement<L>,
+  props: HTMLElementTagNameMap[T] | L,
+  ...children: Node[]
 ) {
-  const el = document.createElement(name);
-  Object.assign(el, props);
-  el.append(...rest);
-  return el;
+  if (typeof name === "string") {
+    const el = document.createElement(name);
+    Object.assign(el, props);
+    el.append(...children);
+    return el;
+  }
+  name({ ...(props as L), children });
 }
