e6c4c691694c476f4a238a1018da24949cfac53e
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-01-18 16:48:16 -0500

    fix: not working for function elements

--- index.ts
+++ index.ts
@@ -8,13 +8,20 @@ export interface FunctionElement<P> {
 export function createElement<T extends keyof HTMLElementTagNameMap, L>(
   name: T | FunctionElement<L>,
   props: HTMLElementTagNameMap[T] | L,
-  ...children: Node[]
+  ...children: any[]
 ) {
   if (typeof name === "string") {
     const el = document.createElement(name);
     Object.assign(el, props);
-    el.append(...children);
+    el.append(...flat(children));
     return el;
   }
-  name({ ...(props as L), children });
+  return name({ ...(props as L), children });
+}
+
+function flat(arr: any[]): any[] {
+  return arr.reduce<any[]>(
+    (acc, value) => acc.concat(Array.isArray(value) ? flat(value) : [value]),
+    []
+  );
 }
